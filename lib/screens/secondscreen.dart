2f155cc4818bcdefc2b1c2f9832900c331ac82b8
Wahid Khan <khanwahidulhaq@gmail.com>
2021-04-26 15:47:19 +0530

    Completed splashscreen.dart

---
+++ lib/screens/secondscreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class secondscreen extends StatefulWidget {
+  @override
+  _secondscreenState createState() => _secondscreenState();
+}
+
+class _secondscreenState extends State<secondscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
